b6f00e4289a8dfe82e27c0faef5ef0689ec5271b
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2026-03-07 12:36:57 +1300

    Fix CI

--- client.py
+++
@@ -1,8 +0,0 @@
-from urllib3 import PoolManager
-from typing import Any, Dict, Optional, List
-from pydantic import BaseModel, Field
-
-class Client:
-    def __init__(self, base_url: str):
-        self.base_url = base_url
-        self.http = PoolManager()
